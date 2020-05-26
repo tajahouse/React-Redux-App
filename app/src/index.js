f2763583d59056a954d67c6bcf62946f22dcf19f
tajahouse <ahlliahtaja@yahoo.com>
2020-05-25 22:01:54 -0700

    Completed adding dependencies and setting up thunk and logger.

--- app/src/index.js
+++ app/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+import * as serviceWorker from './serviceWorker';
+import App from './App';
+
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+const store = createStore(reducer, applyMiddleware(thunk, logger))
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>  <App /> </Provider>
+  
   </React.StrictMode>,
   document.getElementById('root')
 );
